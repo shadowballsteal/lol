cbc0297113ca2822a34932762a7c936aec6e1aa3
shadowballsteal <78164889+shadowballsteal@users.noreply.github.com>
2021-01-29 17:10:12 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function showContact() {
-    if (document.getElementById("discordcontact").style.opacity == "1") {
-        document.getElementById("discordcontact").style.opacity = "0";
-        document.getElementById("discordcontact").style.userSelect = "none";
-    } else {
-        document.getElementById("discordcontact").style.opacity = "1";
-        document.getElementById("discordcontact").style.userSelect = "text";
-    }
-}
